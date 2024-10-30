215f41a144eafd7d59e77ec65be84e67081a8354
deka galang <takikta1771@gmail.com>
2024-10-30 13:29:37 +0000

    fix firebase emulator

--- src/core/app.ts
+++ src/core/app.ts
@@ -1,10 +1,10 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
 import userRoutes from "../routes/userRoutes";
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(bodyParser.json());
 
